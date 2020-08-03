a6eb92bdaec7ed5ed82a87a60a9a171a4b69bcb5
Ivan <followhappyq@gmail.com>
2020-08-03 22:30:18 +0200

    added playagain functionality playAgain

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {
   gameField,
   playerName,
   playAgainButton,
+  result,
 } from "./document"
 import { DEFAULT_BASE_VALUE, BaseValueType, CardValues, scoreForWin } from "./const"
 
@@ -41,6 +42,16 @@ const getDeckId: () => void = async () => {
   if (res.ok) {
     const data = await res.json()
     deckId = data.deck_id
+  } else {
+    console.log(res.status)
+  }
+}
+
+const shuffleCard = async (deckId) => {
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`
+  const res = await fetch(url)
+  if (res.ok) {
+    console.log("The cards are shuffled")
   } else {
     console.log(res.status)
   }
@@ -84,7 +95,7 @@ const twoAces: (card1: cardType, card2: cardType) => boolean = (card1, card2) =>
 
 const addPlayerCard: (player: number, card: cardType) => void = (player, card) => {
   playersDetails[player].cards.push(card)
-  /* const some: CardValues = CardValues[CardValues[card.value.toLowerCase()]]  Не работает.*/
+  /* const some: CardValues = CardValues[CardValues.[card.value.toLowerCase()]] */
   const cardTitle: any = card.value.toLowerCase()
   playersDetails[player].score += getCardValue(cardTitle)
   changePlayerScore(player)
@@ -116,6 +127,20 @@ const onGameOver: () => void = () => {
   getCardButton.classList.add("hidden")
   playerEndButton.classList.add("hidden")
   cardList.classList.add("hidden")
+
+  playersDetails.map((player) => {
+    result.insertAdjacentHTML(
+      "beforeend",
+      `<p style="color:#fff">Player ${player.id + 1} has ${player.score} score.</p>`
+    )
+  })
+  result.insertAdjacentHTML("beforeend", `<h2 style="color:#fff"> Winner</h2>`)
+  scoring(playersDetails, isPositiveOrZero).map((player) => {
+    result.insertAdjacentHTML(
+      "beforeend",
+      `<p style="color:#fff">Player ${player.id + 1} won, with ${player.score} score.</p>`
+    )
+  })
 }
 
 const nextPlayer: () => void = () => {
@@ -133,6 +158,26 @@ const nextPlayer: () => void = () => {
   }
 }
 
+const scoring: (playerDetails: Array<IplayersDetails>, isPositiveOrZer: boolean) => Array<IplayersDetails> = (
+  playersDetails,
+  isPositiveOrZero
+) => {
+  let sortedArray
+  let winnerArray = []
+  if (isPositiveOrZero) {
+    sortedArray = playersDetails.filter((player) => player.score - 21 <= 0).sort(sortArray)
+    winnerArray.push(sortedArray[0])
+  } else {
+    sortedArray = playersDetails.filter((player) => player.score - 21 > 0).sort(sortArray)
+    winnerArray.push(sortedArray[sortedArray.length - 1])
+  }
+  return sortedArray.filter((item) => item.score === winnerArray[0].score)
+}
+
+const sortArray = (player1, player2) => {
+  return player2.score - player1.score
+}
+
 const lastPlayerWon: () => void = () => {
   isPositiveOrZero = true
 }
@@ -140,6 +185,13 @@ const lastPlayerWon: () => void = () => {
 const playAgain: () => void = () => {
   playersDetails.length = 0
   playersDetails.push({ id: 0, cards: [], score: 0 })
+  player = 0
+  isPositiveOrZero = false
+  cardList.innerHTML = ""
+  result.innerHTML = ""
+  cardList.classList.remove("hidden")
+  playAgainButton.classList.add("hidden")
+  onNewGameStarted()
 }
 
 getCardButton.addEventListener("click", () => {
